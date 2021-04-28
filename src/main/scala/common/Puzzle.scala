09b034ddcda343e2c6c5158b927e626ee2627e52
Tyler Romeo <tylerjromeo@gmail.com>
2021-04-28 15:31:18 -0400

    Solve day one

--- src/main/scala/common/Puzzle.scala
+++ src/main/scala/common/Puzzle.scala
@@ -6,11 +6,10 @@ abstract class Puzzle(day: Int) {
   def run(): Unit = {
     WebInputReader.getInput(day) match {
       case Left(e) => println(s"Failed: $e")
-      case Right(input) => {
+      case Right(input) =>
         validateInput(input)
         println(s"Part one's answer is ${solvePart1(input)}")
         println(s"Part two's answer is ${solvePart2(input)}")
-      }
     }
   }
 
@@ -18,14 +17,14 @@ abstract class Puzzle(day: Int) {
   /**
    * read the problem's input and crash hard if it's invalid
    *
-   * @param input
+   * @param input the string of input coming from advent of code
    */
   def validateInput(input: String): Unit
 
   /**
    * solve part 1 of the day's problem
    *
-   * @param input
+   * @param input the string of input coming from advent of code
    * @return
    */
   def solvePart1(input: String): String
@@ -33,7 +32,7 @@ abstract class Puzzle(day: Int) {
   /**
    * solve part 2 of the day's problem
    *
-   * @param input
+   * @param input the string of input coming from advent of code
    * @return
    */
   def solvePart2(input: String): String
--- src/main/scala/day1/DayOne.scala
+++ src/main/scala/day1/DayOne.scala
@@ -17,7 +17,7 @@ class DayOne extends Puzzle(1) {
   /**
    * read the problem's input and crash hard if it's invalid
    *
-   * @param input
+   * @param input the string of input coming from advent of code
    */
   override def validateInput(input: String): Unit = {
     assert(parseInput(input).forall(_.isInstanceOf[Int]))
@@ -26,19 +26,30 @@ class DayOne extends Puzzle(1) {
   /**
    * solve part 1 of the day's problem
    *
-   * @param input
+   * @param input the string of input coming from advent of code
    * @return
    */
   override def solvePart1(input: String): String = {
     val numbers = parseInput(input)
-    numbers.toString()
+    val numbersToMultiply = numbers.combinations(2).find(_.sum == 2020)
+    numbersToMultiply match {
+      case None => "FAILED TO FIND MATCH"
+      case Some(l) => l.product.toString
+    }
   }
 
   /**
    * solve part 2 of the day's problem
    *
-   * @param input
+   * @param input the string of input coming from advent of code
    * @return
    */
-  override def solvePart2(input: String): String = ???
+  override def solvePart2(input: String): String = {
+    val numbers = parseInput(input)
+    val numbersToMultiply = numbers.combinations(3).find(_.sum == 2020)
+    numbersToMultiply match {
+      case None => "FAILED TO FIND MATCH"
+      case Some(l) => l.product.toString
+    }
+  }
 }
